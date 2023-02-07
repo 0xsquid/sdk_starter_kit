e67d67423a815bc88130be6e064032c2384d2fae
Koda <12773720+0x0Koda@users.noreply.github.com>
2023-02-07 12:17:14 +1100

    chore: updated comments

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ if (!privateKey)
   // instantiate the SDK
 
   const squid = new Squid({
-    baseUrl: "https://api.0xsquid.com", // for testnet use "https://testnet.api.0xsquid.com"
+    baseUrl: "https://testnet.api.0xsquid.com", // for testnet use "https://testnet.api.0xsquid.com"
   });
 
   // init the SDK
@@ -25,12 +25,12 @@ if (!privateKey)
   const signer = new ethers.Wallet(privateKey, provider);
 
   const params = {
-    fromChain: 43114, // Goerli testnet
-    fromToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", // WETH on Goerli
-    fromAmount: "5000000000000000", // 0.05 WETH
-    toChain: 137, // Avalanche Fuji Testnet
-    toToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", // aUSDC on Avalanche Fuji Testnet
-    toAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22", // the recipient of the trade
+    fromChain: 43114,
+    fromToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    fromAmount: "5000000000000000",
+    toChain: 137,
+    toToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    toAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22",
     slippage: 3.0, // 3.00 = 3% max slippage across the entire route, acceptable value range is 1-99
     enableForecall: false, // instant execution service, defaults to true
     quoteOnly: false, // optional, defaults to false
